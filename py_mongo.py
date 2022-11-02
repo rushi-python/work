fe6178a9af597daff2db5eafe2e267e71e2232b8
rushi-python <rushi707maha@gmail.com>
2022-11-02 12:27:30 +0530

    try

---
+++ py_mongo.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+
+: eno1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
+    link/ether a4:bb:6d:a9:ad:31 brd ff:ff:ff:ff:ff:ff
+    altname enp0s31f6
+    inet 192.168.1.84/24 brd 192.168.1.255 scope global dynamic noprefixroute eno1
